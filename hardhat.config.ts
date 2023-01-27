37621e28747e6e3b8a9fc1c504efd3e7ee0779b2
Emmanuel <emejia@atx.mx>
2023-01-26 21:56:28 -0600

    networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,24 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+   paths: {
+    artifacts: './artifacts',
+    sources: './contracts',
+    cache: './cache',
+    tests: './test',
+  },
+
+  networks: {
+    ganache: {
+      url: 'HTTP://192.168.137.1:7545'
+    },
+    arbitrum_mainnet: {
+      url: 'https://rpc.ankr.com/arbitrum'
+    },
+    arbitrum_testnet: {
+      url: 'https://goerli-rollup.arbitrum.io/rpc'
+    }
+  }
 };
 
 export default config;
